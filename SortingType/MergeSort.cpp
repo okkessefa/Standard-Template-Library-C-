d17265bc052d93a26f3bb82ae81a22b73d5f4393
okkessefa <oksekesfackn53@gmail.com>
2025-07-28 23:38:29 +0300

    Merge sort algorithm exercise 2

--- SortingType/MergeSort.cpp
+++ SortingType/MergeSort.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
+#include <vector>
 
-void Merge(int Array[], int left, int right, int mid);
-void MergeSort(int Array[], int left, int right){
+
+void Merge(std::vector<int> &Array, int left, int mid, int right){
+     // initialize the control pointer
+    int n1 = mid - left + 1;
+    int n2 = right - mid ;
+
+    if (n1 <= 0 || n2 <= 0) {
+        std::cerr << "Invalid subarray size\n";
+        return;
+    }
+
+     // Creating Temporary Containers
+    std::vector<int> L(n1); 
+    std::vector<int> M(n2);
+
+    //  getting left and right part of array into two piece of array
+    for(int i = 0; i < n1; i++){
+        L[i] = Array[left + i];
+    }
+    for(int j = 0; j < n2; j++){
+        M[j] = Array[mid + 1 + j];
+    }
+
+    // Re-set the controller to re-placement
+    int i = 0; int j = 0 ; int k = left;
+
+    while(i<n1 && j<n2){
+        if(L[i] <= M[j]){
+            Array.at(k) = L.at(i);
+            i++; k++;
+        }else{
+            Array.at(k) = M.at(j);
+            j++; k++;
+        }
+    }// Adding  the elements that if there is reminded element in any container
+    while(i<n1){
+        Array.at(k) = L.at(i);
+        i++; k++;
+    }
+    while(j<n2){
+        Array.at(k) = M.at(j);
+        j++; k++;
+    }
+    // Delete after finishing the re-initalize the variables
+    
+}
+void MergeSort(std::vector<int> &Array, int left, int right){
     if(left < right){// continue to divide the array until being one element
         int mid = left + (right - left) / 2; // Finding the order of middle elemeent
 
@@ -12,19 +58,21 @@ void MergeSort(int Array[], int left, int right){
 
     }
 }
-void PrintArray(int Array[], int size);
-
-int main(){
-
+void PrintArray(std::vector<int> &Array, int size){
+    for (int i = 0; i< size ; i++)
+        std::cout<<Array[i] <<" ";
+    std::cout<<std::endl;
 }
 
 
-// Driver program
+
+// river program
 int main() {
-  int arr[] = {6, 5, 12, 10, 9, 1};
-  int size = sizeof(arr) / sizeof(arr[0]);
-  MergeSort(arr, 0, size - 1);
-  std::cout << "Sorted array: \n";
-  PrintArray(arr, size);
-  return 0;
+    std::vector<int> Array = {6, 5, 12, 10, 9, 1};
+    //int size = sizeof(Array)/sizeof(Array[0]);
+    int size = Array.size(); std::cout<<size<<std::endl;
+    MergeSort(Array, 0, size - 1);
+    std::cout << "Sorted array: \n";
+    PrintArray(Array, size);
+    return 0;
 }
