7a9326bcd3a0016799cd2333f8a68634130a90ad
okkessefa <oksekesfackn53@gmail.com>
2025-08-01 18:22:21 +0300

    Merrge function adding

--- SortingType/projects/sort_benchmark.cpp
+++ SortingType/projects/sort_benchmark.cpp
@@ -9,6 +9,7 @@ void swapelement(int* a, int* b){
     *a = *b;
     *b = temp;
 }
+
 // Sorting algorithms
 int partition(std::vector<int>& arr, int leftmostindex, int rightmostindex){
     int pivot = arr[leftmostindex];
@@ -41,12 +42,65 @@ void quicksort(std::vector<int>& arr, int left, int right){
     }
 }
 
-void mergesort(std::vector<int>& arr, int left, int right);
-void merge(std::vector<int>& arr, int left, int mid, int right);
+void merge(std::vector<int>& arr, int left, int mid, int right){
+
+    int n1 =  mid - left +1;
+    int n2 = right - mid; 
+
+    if (n1 <= 0 || n2 <= 0) {
+        std::cerr << "Invalid subarray size\n";
+        return;
+    }
+
+    std::vector<int> L(n1);
+    std::vector<int> M(n2);
+
+    for(int i = 0; i<n1; i++){
+        L[i] = arr[left + i];
+    }
+
+    for(int j = 0; j<n2; j++){
+        M[j] = arr[mid + 1+ j];
+    }
+
+    int i = 0; int j = 0; int k = left;
+    while((i < n1) && (j < n2)){
+        if(L[i] < M[j]){
+            arr.at(k) = L.at(i);
+            i++; k++;
+        }else{
+            arr.at(k) = M.at(j);
+            j++; k++;
+        }
+    }
+    while(i<n1){
+        arr.at(k) = L.at(i);
+        i++; k++;
+    }
+    while(j<n2){
+        arr.at(k) = M.at(j);
+        j++; k++;
+    }
+
+
+}
+void mergesort(std::vector<int>& arr, int left, int right){
+    if(left<right){
+        int mid = left + (right - left ) /2;
+        
+        mergesort(arr, left, mid);
+        mergesort(arr, mid + 1, right);
+        merge(arr, left , mid, right);
+    }
+}
 
 //Utility
-std::vector<int> generateRandomArray(int size);
-void benchmarkSort(const std::string& name, void(*sortfunc)(std::vector<int>&), std::vector<int> arr);
+std::vector<int> generateRandomArray(int size){
+
+}
+void benchmarkSort(const std::string& name, void(*sortfunc)(std::vector<int>&), std::vector<int> arr){
+
+}
 
 int main(){
     //Menu flow
